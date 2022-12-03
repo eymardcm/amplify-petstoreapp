7d1c65370b91e99f21248a149d94be38c27571ce
Chad <eymardcm@gmail.com>
2022-12-03 18:33:26 -0500

    cleanup

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++
@@ -1,9 +0,0 @@
-export type AmplifyDependentResourcesAttributes = {
-    "api": {
-        "amplifypetstore": {
-            "GraphQLAPIKeyOutput": "string",
-            "GraphQLAPIIdOutput": "string",
-            "GraphQLAPIEndpointOutput": "string"
-        }
-    }
-}
